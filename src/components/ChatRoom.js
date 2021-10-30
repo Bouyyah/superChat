91f3a0574ac6429f53287b963b51e481bb770891
bouyyah <hamameayyoub@gmail.com>
2021-10-30 02:30:50 +0100

    fix photoUrl and change some colors

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -6,12 +6,13 @@ import { FiSend } from 'react-icons/fi';
 function ChatRoom({ db, currentUser, timestamp }) {
 
     const dummy = useRef();
+    
 
     const messagesRef = db.collection('Messages');
     const query = messagesRef.orderBy('createdAt').limit(25);
 
     const [Messages] = useCollectionData(query, {idField: 'id'});
-
+    console.log(Messages);
     const [formValue, setFormValue] =  useState("");
 
     const sendMessage = async (e) => {
@@ -34,7 +35,7 @@ function ChatRoom({ db, currentUser, timestamp }) {
     return (
         <>
             <main>
-                {Messages && Messages.map( msg => <ChatMessage key = { msg.id } message = { msg.text } currentUser = { currentUser }/>)}
+                {Messages && Messages.map( msg => <ChatMessage key = { msg.id } message = { msg.text } uid={ msg.uid } currentUser = { currentUser } photoURL = { msg.photoURL } />)}
 
                 <div ref={dummy}></div>
             </main>
